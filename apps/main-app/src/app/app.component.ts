c1c389fdfc1b3e686ed840222814ca631f19dc99
fleish80 <Fleish.D@gmail.com>
2024-09-22 18:07:37 +0300

    angular basic 0

--- apps/main-app/src/app/app.component.ts
+++ apps/main-app/src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { JokeService } from '../services/joke/joke.service';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Joke } from '../models/joke.model';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, AsyncPipe, JsonPipe],
+  imports: [AsyncPipe],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'main-app';
 
   private jokeService = inject(JokeService);
+  joke$: Observable<Joke>;
 
-  joke$ = this.jokeService.getJoke();
+  constructor() {
+    this.loadAnotherJoke();
+  }
+
+  loadAnotherJoke() {
+    this.joke$ = this.jokeService.getJoke();
+  }
 
 }
